2ca04659d350c5204f87346400efe7cd69230800
tamnm20 <tamnm.b18dt208@stu.ptit.edu.vn>
2022-09-07 01:08:09 +0700

    Update SendHA.go

--- Send_HA/SendHA.go
+++ Send_HA/SendHA.go
@@ -1,3 +1,12 @@
+package main
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+	"strconv"
+	"time"
+)
 // Khai bao
 // IP
 var ip string = ""
@@ -5,6 +14,8 @@ var ip string = ""
 var temp string = ""
 // Mem
 var mem string = ""
+// Disk
+var disk string = ""
 // Cpu
 var user int = 0
 var system int = 0 
@@ -16,48 +27,81 @@ func FloatToString(input_num float64) string {
     return strconv.FormatFloat(input_num, 'f', 3, 64)
 }
 
-//get teamperture of PI
-func Test_temp(){
-	out, err := exec.Command("/opt/vc/bin/vcgencmd", "measure_temp").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", out)
-	
-	out2 := out[0:(len(out)-1)]
-	fmt.Printf("%s\n", out2)
-	temp = string(out2)
-	
+func sleep_ms(ms time.Duration) {
+	time.Sleep(time.Millisecond * ms)   // sleep(nano second)
 }
 
-func GetOutboundIP() net.IP {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close()
-
-    localAddr := conn.LocalAddr().(*net.UDPAddr)
-    ip = localAddr.IP.String()
-    return localAddr.IP
-}
-
-
-//get Memory of PI
-func Get_Mem(){
-	out, err := exec.Command("sh","-c","free -b | grep Mem").Output()
+//get disk of PI
+func get_disk(){
+	out, err := exec.Command("sh","-c","df | grep /dev/mmcblk0p1").Output() // Lấy thông số Disk
 	if err != nil {
 		log.Fatal(err)
 	}
 	var out1 = string(out)
 	var list = strings.Split(out1, " ")
 
-	total,_ := strconv.Atoi(list[6])
-	 used,_ := strconv.Atoi(list[9])
-	 var percent float64 = (float64(used)*100)/float64(total)
+//	fmt.Printf("list : %q\n", list)
 
-	 fmt.Printf("Phan tram used : %T\t%v\n",percent, percent)
-	 mem = FloatToString(percent) 
+	total,_ := strconv.Atoi(list[3])
+	used,_ := strconv.Atoi(list[4])
+
+//	fmt.Printf(" %T\t%v\n",total, total)
+//	fmt.Printf(" %T\t%v\n",used, used)
+
+	var percent float64 = (float64(used)*100)/float64(total)
+//	fmt.Printf("Phan tram used : %T\t%v\n",percent, percent)
+
+	disk = FloatToString(percent)
+}
+
+//get teamperture of PI
+func get_temp(){
+	out, err := exec.Command("sh","-c", "cat /sys/devices/virtual/thermal/thermal_zone0/temp").Output() // Lấy thông số Cpu temp
+	if err != nil {
+			log.Fatal(err)
+	}
+	out1,_ := strconv.Atoi(string(out[0:(len(out)-1)]))
+	out3 := float64(out1)/1000
+	// fmt.Printf("%f\n", out3)
+	temp = FloatToString(out3)
+}
+
+//get IP of PI
+func getIP(){
+	out, err := exec.Command("sh","-c","ifconfig eth0| grep inet").Output() // Lấy thông số ip_pi
+	if err != nil {
+					log.Fatal(err)
+	}
+	var out1 = string(out)
+	var list = strings.Split(out1, " ")
+
+	// fmt.Printf("list : %q\n", list)
+
+	// fmt.Printf(" %T\t%v\n",list[9], list[9])
+	ip = list[9]
+}
+
+//get Memory of PI
+func Get_Mem(){
+	out, err := exec.Command("sh","-c","free -m | grep Mem").Output() // Lấy thông số Memory
+	if err != nil {
+			log.Fatal(err)
+	}
+	var out1 = string(out)
+	var list = strings.Split(out1, " ")
+
+	// fmt.Printf("list : %q\n", list)
+
+	total,_ := strconv.Atoi(list[13])
+	used,_ := strconv.Atoi(list[22])
+
+	// fmt.Printf(" %T\t%v\n",total, total)
+	// fmt.Printf(" %T\t%v\n",used, used)
+
+	var percent float64 = (float64(used)*100)/float64(total)
+	// fmt.Printf("Phan tram used : %T\t%v\n",percent, percent)
+
+	mem = FloatToString(percent)
 }
 
 // Get cpu Pi
@@ -69,7 +113,7 @@ func Get_cpu(){
 	var out1 = string(out)
 	var cpu = strings.Split(out1, " ")
 	
-	fmt.Printf("Old :  %v\t%v\t%v\n",user, system,idle)
+	// fmt.Printf("Old :  %v\t%v\t%v\n",user, system,idle)
 	
 	x,_ := strconv.Atoi(cpu[2])
 	user = x - user
@@ -79,21 +123,22 @@ func Get_cpu(){
 	idle =  z- idle
 	avg = ((float64(user +system))*100)/(float64(user + system + idle))
 	
-	fmt.Printf("New :  %v\t%v\t%v\n",x, y,z)
-	fmt.Printf("Sub :  %v\t%v\t%v\n",user, system,idle)
-	fmt.Printf("percent : %v\n\n", avg)
+	// fmt.Printf("New :  %v\t%v\t%v\n",x, y,z)
+	// fmt.Printf("Sub :  %v\t%v\t%v\n",user, system,idle)
+	// fmt.Printf("percent : %v\n\n", avg)
 		
 	user = x
 	system = y
-	idle = z
-	
-	}
+	idle = z	
+}
 
 // Push data Thingsboard
 func Publish_data(){
-	var payload string = "{" + "\"TEMP\":" + "\"" + temp + "\"" + "," + "IP" + ":" + ip + "," + "\"Memory Pi\":" + "\"" + mem + "\"" + "," + "\"CPU Pi\":" + "\"" + FloatToString(avg) + "\"" + "}" // Ghep các bản tin từ code cũ thành 1 bản tin 
-	mqtt_demo_Thingsboard_Io.Publish(THINGSBOARD_TOPIC_IN, 0, false, payload)
-	mqtt_amazon.Publish(THINGSBOARD_TOPIC_IN, 0, false, payload)
+	var payload string = "{" + "\"TEMP\":" + "\"" + temp + "\"" + "," + "\"IP Pi\":" + "\"" + ip + "\"" + "," + "\"Memory Pi\":" + "\"" + mem + "\"" + "," + "\"CPU Pi\":" + "\"" + FloatToString(avg) + "\"" + "}" // Ghep các bản tin từ code cũ thành 1 bản tin
+	fmt.Printf(payload) 
+	fmt.Printf("\n")
+	//mqtt_demo_Thingsboard_Io.Publish(THINGSBOARD_TOPIC_IN, 0, false, payload)
+	//mqtt_amazon.Publish(THINGSBOARD_TOPIC_IN, 0, false, payload)
 	
 }
 
@@ -101,19 +146,19 @@ func Publish_data(){
 
 func main() {
 
-	mqtt_begin()
-	mqtt_domoticz.Publish(DOMITICZ_TOPIC_IN, 0, false,reset)	// reset buzzer ve off
-	mqtt_domoticz.Subscribe(DOMITICZ_TOPIC_OUT, 0, mqtt_domoticz_messageHandler)  // QoS = 0
-	mqtt_amazon.Subscribe(THINGSBOARD_TOPIC_OUT, 0,mqtt_messageHandler)
+	// mqtt_begin()
+	// mqtt_domoticz.Publish(DOMITICZ_TOPIC_IN, 0, false,reset)	// reset buzzer ve off
+	// mqtt_domoticz.Subscribe(DOMITICZ_TOPIC_OUT, 0, mqtt_domoticz_messageHandler)  // QoS = 0
+	// mqtt_amazon.Subscribe(THINGSBOARD_TOPIC_OUT, 0,mqtt_messageHandler)
 	
 	// Loop:
 	for {
-		sleep_ms(5000) // delay 5s gửi dữu liệu 1 lần
-		GetOutboundIP()
-		Test_temp()
+		sleep_ms(1000) // delay 1s gửi dữu liệu 1 lần
+		getIP()
+		get_temp()
+		get_disk()
 		Get_Mem()
 		Get_cpu()
-		Publish_data()
-		
+		Publish_data()	
 	}
 }
